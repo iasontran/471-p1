82a039de21a6ab28cb8783f14949d851cd6bb447
Jason <ichikasuto@gmail.com>
2018-02-02 16:34:37 -0700

    Create template for decrypt

---
+++ decrypt.py
@@ -0,0 +1,24 @@
+
+def kasiski():
+
+    return kas
+
+def vignere():
+
+    return vig
+
+def shift():
+
+    return shift
+
+def sub():
+
+    return sub
+
+def pad():
+
+    return pad
+
+def perm():
+
+    return perm
--- ic.py
+++ ic.py
@@ -6,10 +6,11 @@ def ioc(file):
     #file = open(sys.argv[1], 'r')
     #ct = file.read()
     ct = file.read()
+    file.seek(0,0)
     icsum = 0.0
 
     n = len(ct)
-    freqs = findnfreqs(ct, 1)
+    freqs = findnfreqs(file, 1)
     for x in freqs.values():
         icsum += x * ((x * n - 1)/(n - 1))
 
