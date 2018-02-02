8abab275a4321c53735983c8a250319498173d3a
Jason <ichikasuto@gmail.com>
2018-02-02 14:48:27 -0700

    Add gitignore

--- ic.py
+++ ic.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
 import sys
+from freq import *
 
 
-def ic():
+def ic(freq):
     file = open(sys.argv[1], 'r')
     ct = file.read()
-    freq = {}
     icsum = 0.0
 
     n = len(ct)
